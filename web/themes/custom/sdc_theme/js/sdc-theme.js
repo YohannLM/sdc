1f1956868e0bffae79b70cf6829dc6c1aa0de8e0
yohann le magueresse <yohann.le-magueresse@conserto.pro>
2024-04-22 14:19:24 +0200

    init repo

---
+++ web/themes/custom/sdc_theme/js/sdc-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Single Directory Components behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.sdcTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
